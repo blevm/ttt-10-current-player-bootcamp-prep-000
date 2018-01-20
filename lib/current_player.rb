9f36ed102a05f36ec8fe0adfdeacb6adbd0b959d
Michelle B <mjblevins7@gmail.com>
2018-01-20 04:40:49 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count(board)
+  turn_counter = 0
+  board.each do |index|
+    if index = "X" || index = "O"
+    turn_counter += 1
+    puts "#{turn_counter}"
+  end    
+end
+
+
+def current_player(board)
+  board.each do |index|
+    if index = "x" || index = "o"
+    turn_counter += 1
+    puts "#{turn_counter}"
+  end  
+end
